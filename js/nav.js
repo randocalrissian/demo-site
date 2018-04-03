021b97148f0f10485117e41df3cdc58ab2544da9
Randy <ranmcd1@gmail.com>
2018-04-03 08:30:27 -0500

    Fixing nav issues. I shouldn't be doing this with master branch, but I'm having fun.

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,5 @@
 (function($) {
   "use strict"; 
-
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -8,16 +7,17 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
+          scrollTop: (target.offset().top - 60)
         }, 1000, "easeInOutExpo");
         return false;
       }
     }
   });
-})(jQuery); 
-$(document).ready(function(){
+  // Navigation overlay toggle
+  $(document).ready(function(){
     $(".toggle-menu").click(function(){
         $("#navOverlay").toggle(250);
         $("#navToggle").toggle(250);
     });
-});
+  });
+})(jQuery); 
